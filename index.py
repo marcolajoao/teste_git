312c7f4a744326c4e296da5fcce2958cdc793223
marcolajoao <jp.joaopedromarcola@hotmail.com>
2025-04-13 19:23:58 -0300

    acrecimo de booleano

--- index.py
+++ index.py
@@ -3,3 +3,10 @@ print('Hello Word')
 nome = input('Qual é seu nome ladrão? \n')
 
 print(f'Ola {nome} como voce ta ladrão??')
+
+senha = input('Defina uma senha ai')
+
+confirma_senha = input('COnfirma ai só pra eu ver um negocio...')
+
+if senha == confirma_senha:
+    print("senha correta!")
